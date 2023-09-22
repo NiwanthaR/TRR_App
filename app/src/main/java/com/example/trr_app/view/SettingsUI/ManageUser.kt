224917874ab81f283b8d9df73e242551a1301187
Niwantha Ranawaka <niwantharanawaka@ymail.com>
2023-09-22 09:12:24 +0530

    Manage user dashboard

---
+++ app/src/main/java/com/example/trr_app/view/SettingsUI/ManageUser.kt
@@ -0,0 +1,12 @@
+package com.example.trr_app.view.SettingsUI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trr_app.R
+
+class ManageUser : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_manage_user)
+    }
+}
