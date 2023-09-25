2e8ce5f3431854c0302ddba4b00ed42378e4ba36
Niwantha Ranawaka <niwantharanawaka@ymail.com>
2023-09-25 16:51:58 +0530

    Add Booking Start

---
+++ app/src/main/java/com/example/trr_app/view/ManageUI/AddBooking.kt
@@ -0,0 +1,12 @@
+package com.example.trr_app.view.ManageUI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.trr_app.R
+
+class AddBooking : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_booking)
+    }
+}
