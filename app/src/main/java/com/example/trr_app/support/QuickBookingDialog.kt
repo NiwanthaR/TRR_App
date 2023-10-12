3482cca0c099555828bf5da096d2b44a2a1276f6
Niwantha Ranawaka <44175668+NiwanthaR@users.noreply.github.com>
2023-10-12 23:24:01 +0530

    Quick Booking 02

--- app/src/main/java/com/example/trr_app/support/QuickBookingDialog.kt
+++ app/src/main/java/com/example/trr_app/support/QuickBookingDialog.kt
@@ -1,13 +1,19 @@
 package com.example.trr_app.support
 
+import android.app.Dialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.trr_app.R
+import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 
 class QuickBookingDialog: BottomSheetDialogFragment() {
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        return super.onCreateDialog(savedInstanceState)
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.dialog_quick_booking, container, false)
 
@@ -25,5 +31,14 @@ class QuickBookingDialog: BottomSheetDialogFragment() {
 //        }
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //val bottomSheetBehavior = BottomSheetBehavior.from((view))
+        //bottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+
+
     }
 }
--- app/src/main/java/com/example/trr_app/view/Dashboard.kt
+++ app/src/main/java/com/example/trr_app/view/Dashboard.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.FrameLayout
 import android.widget.LinearLayout
 import android.widget.RelativeLayout
 import com.example.trr_app.R
@@ -22,12 +23,15 @@ class Dashboard : BaseActivity(),OnClickListener {
     private val quickBookingDashboard : RelativeLayout
         get() = findViewById(R.id.layoutQuickBooking)
 
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_dashboard)
 
         manageUIDashboard.setOnClickListener(this)
         quickBookingDashboard.setOnClickListener(this)
+
     }
 
     override fun onClick(view: View?) {
@@ -38,21 +42,20 @@ class Dashboard : BaseActivity(),OnClickListener {
     }
 
     private fun showBottomDialog(){
-//            val bottomSheetDialog = BottomSheetDialog(this)
-////            bottomSheetDialog.setContentView(R.layout.dialog_quick_booking)
+            val bottomSheetDialog = BottomSheetDialog(this)
+            bottomSheetDialog.setContentView(R.layout.dialog_quick_booking)
 ////
 ////            //val copy = bottomSheetDialog.findViewById<LinearLayout>(R.id.copyLinearLayout)
 ////            //val share = bottomSheetDialog.findViewById<LinearLayout>(R.id.shareLinearLayout)
 ////            //val upload = bottomSheetDialog.findViewById<LinearLayout>(R.id.uploadLinearLayout)
 ////            //val download = bottomSheetDialog.findViewById<LinearLayout>(R.id.download)
 ////            //val delete = bottomSheetDialog.findViewById<LinearLayout>(R.id.delete)
-////
-////            bottomSheetDialog.show()
+        val standardBottomSheetBehavior = BottomSheetBehavior.from(bottomSheetDialog.findViewById<FrameLayout>(R.id.quickBookingFrameLayout)!!)
+        standardBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
+        bottomSheetDialog.show()
 
-        val bottomSheet = QuickBookingDialog()
-//        val modalBottomSheetBehavior = (bottomSheet.dialog as BottomSheetDialog).behavior
-//        modalBottomSheetBehavior.state = BottomSheetBehavior.STATE_EXPANDED
-        bottomSheet.show(supportFragmentManager, "ModalBottomSheet")
+        //val bottomSheet = QuickBookingDialog()
+        //bottomSheet.show(supportFragmentManager, "ModalBottomSheet")
 
     }
 }
