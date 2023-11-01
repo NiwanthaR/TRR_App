91d82ee9117ef2f37c95d606e1dce0b546cf50c2
Niwantha Ranawaka <niwantharanawaka@ymail.com>
2023-11-01 15:49:14 +0530

    Quick Book Layout Modify

--- app/src/main/java/com/example/trr_app/view/ManageUI/QuickBookingActivity.kt
+++ app/src/main/java/com/example/trr_app/view/ManageUI/QuickBookingActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.View.OnClickListener
+import android.widget.LinearLayout
 import android.widget.RelativeLayout
+import android.widget.TextView
 import androidx.core.util.Pair
 import com.example.trr_app.R
 import com.example.trr_app.common.BaseActivity
 import com.example.trr_app.holders.BookingViewHolder
+import com.example.trr_app.model.RoomDetails
 import com.example.trr_app.model.RoomReserve
 import com.example.trr_app.model.SubmitBooking
 import com.firebase.ui.database.FirebaseRecyclerAdapter
@@ -22,6 +25,7 @@ import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.getValue
 import java.text.SimpleDateFormat
 import java.util.Calendar
 import java.util.Locale
@@ -61,12 +65,79 @@ class QuickBookingActivity : BaseActivity(),OnClickListener {
     private val searchDateInput : TextInputEditText
         get() = findViewById(R.id.tv_searchDates)
 
+    //room name
+    private val roomName01 : TextView
+        get() = findViewById(R.id.roomName1)
+    private val roomType01 : TextView
+        get() = findViewById(R.id.roomType01)
+    private val roomCapacity01 : TextView
+        get() = findViewById(R.id.roomCapacity01)
+    private val roomCost01 : TextView
+        get() = findViewById(R.id.roomCost01)
+
+    private val roomName02 : TextView
+        get() = findViewById(R.id.roomName2)
+    private val roomType02 : TextView
+        get() = findViewById(R.id.roomType02)
+    private val roomCapacity02 : TextView
+        get() = findViewById(R.id.roomCapacity02)
+    private val roomCost02 : TextView
+        get() = findViewById(R.id.roomCost02)
+
+    private val roomName03 : TextView
+        get() = findViewById(R.id.roomName3)
+    private val roomType03 : TextView
+        get() = findViewById(R.id.roomType03)
+    private val roomCapacity03 : TextView
+        get() = findViewById(R.id.roomCapacity03)
+    private val roomCost03 : TextView
+        get() = findViewById(R.id.roomCost03)
+
+    private val roomName04 : TextView
+        get() = findViewById(R.id.roomName4)
+    private val roomType04 : TextView
+        get() = findViewById(R.id.roomType04)
+    private val roomCapacity04 : TextView
+        get() = findViewById(R.id.roomCapacity04)
+    private val roomCost04 : TextView
+        get() = findViewById(R.id.roomCost04)
+
+    private val roomName05 : TextView
+        get() = findViewById(R.id.roomName5)
+    private val roomType05 : TextView
+        get() = findViewById(R.id.roomType05)
+    private val roomCapacity05 : TextView
+        get() = findViewById(R.id.roomCapacity05)
+    private val roomCost05 : TextView
+        get() = findViewById(R.id.roomCost05)
+
+    private val roomName06 : TextView
+        get() = findViewById(R.id.roomName6)
+    private val roomType06 : TextView
+        get() = findViewById(R.id.roomType06)
+    private val roomCapacity06 : TextView
+        get() = findViewById(R.id.roomCapacity06)
+    private val roomCost06 : TextView
+        get() = findViewById(R.id.roomCost06)
+
+    private val roomName07 : TextView
+        get() = findViewById(R.id.roomName7)
+    private val roomType07 : TextView
+        get() = findViewById(R.id.roomType07)
+    private val roomCapacity07 : TextView
+        get() = findViewById(R.id.roomCapacity07)
+    private val roomCost07 : TextView
+        get() = findViewById(R.id.roomCost07)
+
+    private val roomLoadLayout : LinearLayout
+        get() = findViewById(R.id.roomLoadingLayout)
+
     //date string
     private var startDate:String? = null
     private var endDate:String? = null
 
     private val contentView : RelativeLayout
-        get() = findViewById(R.id.quickBookLayout)
+        get() = findViewById(R.id.quickBookingGuid)
 
     private var options: FirebaseRecyclerOptions<SubmitBooking>? = null
     private var adapter: FirebaseRecyclerAdapter<SubmitBooking, BookingViewHolder>? = null
@@ -79,6 +150,8 @@ class QuickBookingActivity : BaseActivity(),OnClickListener {
 
         searchDateInput.setOnClickListener(this)
 
+        //load room data
+        loadRoomData()
     }
 
     private fun showDatePicker(){
@@ -159,6 +232,78 @@ class QuickBookingActivity : BaseActivity(),OnClickListener {
         })
     }
 
+    private fun loadRoomData(){
+        //database reference
+        databaseReference = firebaseDatabaseReference.child("Room Details").child("Room Profile")
+
+        val roomList : ArrayList<RoomDetails> = ArrayList()
+
+        databaseReference.addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                //clean array
+                roomList.clear()
+
+                for(postSnapshot in snapshot.children){
+                    val roomDetails:RoomDetails? = postSnapshot.getValue(RoomDetails::class.java)
+                    if (roomDetails!=null){
+                        Log.e(TAG, "Data loading success")
+                        roomList.add(roomDetails)
+                    }else{
+                        Log.e(TAG, "Data loading failed. room is empty")
+                    }
+                }
+
+                val roomListSize = roomList.size
+                if (roomListSize==7){
+                    setData(roomList)
+                }else{
+                    Log.e(TAG, "Data loading failed. room data is empty")
+                    Snackbar.make(contentView,R.string.booking_data_null, Snackbar.LENGTH_SHORT).show()
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
+    }
+
+    private fun setData(roomList: ArrayList<RoomDetails>){
+        roomName01.text = roomList[0].room_name
+        roomName02.text = roomList[1].room_name
+        roomName03.text = roomList[2].room_name
+        roomName04.text = roomList[3].room_name
+        roomName05.text = roomList[4].room_name
+        roomName06.text = roomList[5].room_name
+        roomName07.text = roomList[6].room_name
+
+        roomCapacity01.text = roomList[0].room_capacity
+        roomCapacity02.text = roomList[1].room_capacity
+        roomCapacity03.text = roomList[2].room_capacity
+        roomCapacity04.text = roomList[3].room_capacity
+        roomCapacity05.text = roomList[4].room_capacity
+        roomCapacity06.text = roomList[5].room_capacity
+        roomCapacity07.text = roomList[6].room_capacity
+
+        roomCost01.text = roomList[0].room_cost
+        roomCost02.text = roomList[1].room_cost
+        roomCost03.text = roomList[2].room_cost
+        roomCost04.text = roomList[3].room_cost
+        roomCost05.text = roomList[4].room_cost
+        roomCost06.text = roomList[5].room_cost
+        roomCost07.text = roomList[6].room_cost
+
+        roomType01.text = roomList[0].room_condition
+        roomType02.text = roomList[1].room_condition
+        roomType03.text = roomList[2].room_condition
+        roomType04.text = roomList[3].room_condition
+        roomType05.text = roomList[4].room_condition
+        roomType06.text = roomList[5].room_condition
+        roomType07.text = roomList[6].room_condition
+    }
+
     private fun checkAvailabilityRooms(overLapList: ArrayList<SubmitBooking>){
         val roomBookingList : ArrayList<RoomReserve> = ArrayList()
         val listSize = overLapList.size
@@ -223,6 +368,7 @@ class QuickBookingActivity : BaseActivity(),OnClickListener {
         }
         if (roomFlag07){
             rmCard07.visibility = View.GONE
+            roomLoadLayout.visibility = View.GONE
         }else{
             rmCard07.visibility = View.VISIBLE
         }
